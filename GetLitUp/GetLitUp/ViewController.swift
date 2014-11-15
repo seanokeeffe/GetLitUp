8d5cec7fdb4bc43c6e7ef551dd320226be32acf6
Jenner <jfelton@g.hmc.edu>
2014-11-15 07:30:35 -0800

    Added menu closing options.
    Just getting it to owkr.

--- GetLitUp/GetLitUp/ViewController.swift
+++ GetLitUp/GetLitUp/ViewController.swift
@@ -9,17 +9,25 @@
 import UIKit
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        stadiumName.delegate = self
+        sectionNumber.delegate = self
+        seatNumber.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        self.view.endEditing(true);
+        return false;
     }
 
     
