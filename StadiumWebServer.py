6922f88e42df87ab59617211155247ecc9681be4
seanokeeffe <seanokeeffe28@gmail.com>
2014-11-15 20:58:29 -0800

    Updated web server
    I guess the other one was a previous version. Now it is working.

--- StadiumWebServer.py
+++ StadiumWebServer.py
@@ -1,23 +1,24 @@
 from flask import Flask
+import time
 app = Flask(__name__)
 
-def getLightPattern(stadium, seciton, seat):
-	if (stadium == 'Stadium1'):
-		if (section == '1'):
-			if (seat == '1A'):
-				return '1,0,1,0,1,1,0,0,0,1,0,1,0'
-			elif (seat == '1B'):
-				return '1,0,1,0,1,0,1,0,0,1,0,1,0'
-			elif (seat == '2A'):
-				return '0,1,0,1,0,0,0,1,0,1,0,1,0'
-			elif (seat == '2B'):
-				return '0,1,0,1,0,0,0,0,1,1,0,1,0'
-			else:
-				return 'Invalid seat: ' + seat
-		else:
-			return 'Invalid section: ' + section
-	else:
-		return 'Invalid stadium: ' + stadium
+def getLightPattern(stadium, section, seat):
+    if (stadium == 'Stadium1'):
+        if (section == '1'):
+            if (seat == '1A'):
+                return '1,0,1,0,1,1,0,0,0,1,0,1,0'
+            elif (seat == '1B'):
+                return '1,0,1,0,1,0,1,0,0,1,0,1,0'
+            elif (seat == '2A'):
+                return '0,1,0,1,0,0,0,1,0,1,0,1,0'
+            elif (seat == '2B'):
+                return '0,1,0,1,0,0,0,0,1,1,0,1,0'
+            else:
+                return 'Invalid seat: ' + seat
+        else:
+            return 'Invalid section: ' + section
+    else:
+        return 'Invalid stadium: ' + stadium
 
 
 @app.route("/")
@@ -26,16 +27,16 @@ def hello():
 
 @app.route("/data/<stadium>/<section>/<seat>")
 def getLightData(stadium, section, seat):
-	# Set the start time and get epoch form
-	startTime = '15.11.2014 7:30:00'
-	pattern = '%d.%m.%Y %H:%M:%S'
-	epochStart = int(time.mktime(time.strptime(startTime, pattern)))
+    # Set the start time and get epoch form
+    startTime = '15.11.2014 7:30:00'
+    pattern = '%d.%m.%Y %H:%M:%S'
+    epochStart = int(time.mktime(time.strptime(startTime, pattern)))
 
-	# Get the light pattern for the given seating info
-	lightPattern = getLightPattern(stadium, seciton, seat)
+    # Get the light pattern for the given seating info
+    lightPattern = getLightPattern(stadium, section, seat)
 
-	return str(epochStart) + ":" + lightPattern
+    return str(epochStart) + ":" + lightPattern
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', Debug=True)
+    app.run(host='0.0.0.0', debug=True)
